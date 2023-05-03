1690932e51218441eeeb2fb425000f77ed21d019
Jack Warner <21jackwarner@gmail.com>
2023-05-02 20:20:33 -0400

    fixing analyzer

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -70,29 +70,31 @@ class Game:
 # # Show the results in narrow form
 # print(game.show_results(form='narrow'))
 
-import pandas as pd
-
 class Analyzer:
     def __init__(self, game):
         self.game = game
         self.dtype = type(game.dice[0].faces[0])
-        
-    def jackpot(self):
-        jackpot_count = (self.game.results == self.game.results.iloc[:, 0]).all(axis=1).sum()
+
+    def jackpot(self, face_idx=0):
+        jackpot_count = (self.game.results == self.game.results.iloc[:, face_idx]).all(axis=1).sum()
         self.jackpot_results = pd.DataFrame({'Jackpot Count': jackpot_count}, index=[0])
         return jackpot_count
-        
+
     def combo(self):
         combo_counts = self.game.results.apply(lambda x: len(set(x)), axis=1).value_counts().sort_index()
         combo_index = pd.MultiIndex.from_tuples([(i, ) for i in range(1, len(self.game.dice) + 1)])
         combo_results = pd.DataFrame(combo_counts, columns=['Combo Count'], index=combo_index)
         self.combo_results = combo_results
         return combo_results
-        
+
     def face_counts_per_roll(self):
+        num_dice = len(self.game.dice[0].faces)
         face_counts = pd.DataFrame(0, index=self.game.results.index, columns=self.game.dice[0].faces)
-        for face in self.game.dice[0].faces:
-            face_counts[face] = self.game.results.apply(lambda x: x.tolist().count(face), axis=1)
+        for i in range(len(self.game.dice)):
+            if len(self.game.dice[i].faces) != num_dice:
+                raise ValueError("All dice must have the same number of faces")
+            for face in self.game.dice[i].faces:
+                face_counts[(i, face)] = self.game.results.iloc[:, i].apply(lambda x: x.count(face))
         self.face_counts_results = face_counts
         return face_counts
 
